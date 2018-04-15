bf98ff9d5748d0704ce84bd9a6dc458e9ff0e722
hickeyma75 <hickeyma@dukes.jmu.edu>
2018-04-15 17:40:04 -0400

    Updates to the sql commands.  Need to implement the gui

--- CS474Project3Driver.java
+++ CS474Project3Driver.java
@@ -1,1 +1,29 @@
-//Hello World
+/**
+ * Orders Program that interacts with the database in order to insert new customers and to get all
+ * of the customer information
+ * 
+ * @author Vega, Raasio
+ * Group Name: Nunchuck Secret Agents
+ *
+ */
+public class CS474Project3Driver {
+
+  /**
+   * main presents the UI for the program and then interacts with the MySQLAccess file to get info
+   * from the DB
+   * 
+   * @author Vega, Raasio
+   * @param args
+   * @throws Exception
+   */
+  public static void main(String[] args) throws Exception {
+    MySQLAccess dao = new MySQLAccess();
+    dao.connectToDB();
+    dao.getChantIDs("Rome", null, "Christmas");
+    // Scanner to pull information from the user to give to the program
+
+  }
+
+
+
+}
